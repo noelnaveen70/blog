645c9444b0a27c02d64774f90a1955386f20dfec
noelnaveen70 <noelnaveen70@gmail.com>
2025-01-22 17:15:04 +0530

    second

--- BlogAppFrontendBackend/src/App.jsx
+++ BlogAppFrontendBackend/src/App.jsx
@@ -4,8 +4,7 @@ import Login from './components/Login'
 import { Route, Routes } from 'react-router-dom'
 import Signup from './components/Signup'
 import AddBlog from './components/AddBlog'
-import Navbar from './components/Navbar'
-import Main from './components/main'
+import Main from './components/Main'
 import PrivateRoutes from './components/PrivateRoutes'
 
 const App = () => {
